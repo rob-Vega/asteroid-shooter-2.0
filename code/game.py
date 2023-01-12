e2920df828a4b8cf1d307d76e1229b1670d9c613
Roberto Vega <rvegag95@gmail.com>
2023-01-12 01:29:57 -0300

    completed

--- code/game.py
+++ code/game.py
@@ -6,9 +6,14 @@ class Ship(pygame.sprite.Sprite):
         super().__init__(groups)
         self.image = pygame.image.load('../graphics/ship.png').convert_alpha()
         self.rect = self.image.get_rect(center = (WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2))
+        self.mask = pygame.mask.from_surface(self.image)
+        
         # timer
         self.can_shoot = True
         self.shoot_time = None
+
+        # sound
+        self.laser_sound = pygame.mixer.Sound('../sounds/laser.ogg')
 
     def input_position(self):
         pos = pygame.mouse.get_pos()
@@ -22,13 +27,14 @@ class Ship(pygame.sprite.Sprite):
 
     def laser_shot(self):
         if pygame.mouse.get_pressed()[0] and self.can_shoot:
+            self.laser_sound.play()
             self.can_shoot = False
             self.shoot_time = pygame.time.get_ticks()
 
             Laser(laser_group, self.rect.midtop)
 
     def meteor_collision(self):
-        if pygame.sprite.spritecollide(self, meteor_group, False):
+        if pygame.sprite.spritecollide(self, meteor_group, False, pygame.sprite.collide_mask):
             pygame.quit()
             sys.exit()
 
@@ -43,14 +49,21 @@ class Laser(pygame.sprite.Sprite):
         super().__init__(groups)
         self.image = pygame.image.load('../graphics/laser.png').convert_alpha()
         self.rect = self.image.get_rect(midbottom = pos)
+        self.mask = pygame.mask.from_surface(self.image)
+
         # float based position
         self.pos = pygame.math.Vector2(self.rect.topleft)
         self.direction = pygame.math.Vector2(0, -1)
         self.speed = 600
 
+        # sound
+        self.explosion_sound = pygame.mixer.Sound('../sounds/explosion.wav')
+        self.explosion_sound.set_volume(0.1)
+
     def meteor_collision(self):
-        if pygame.sprite.spritecollide(self, meteor_group, True):
+        if pygame.sprite.spritecollide(self, meteor_group, True, pygame.sprite.collide_mask):
             self.kill()
+            self.explosion_sound.play()
 
     def update(self):
         # self.rect.y -= 10
@@ -63,12 +76,15 @@ class Laser(pygame.sprite.Sprite):
 class Meteor(pygame.sprite.Sprite):
     def __init__(self, groups, pos):
         super().__init__(groups)
+
         # rnadomize meteor size
         surface = pygame.image.load('../graphics/meteor.png').convert_alpha()
         size = pygame.math.Vector2(surface.get_size()) * uniform(0.5, 1.5)
         self.scale_surf = pygame.transform.scale(surface, size)
         self.image = self.scale_surf
         self.rect = self.image.get_rect(midbottom = pos)
+        self.mask = pygame.mask.from_surface(self.image)
+
         # float based position
         self.pos = pygame.math.Vector2(self.rect.center)
         self.direction = pygame.math.Vector2(uniform(-0.5, 0.5), 1)
@@ -83,12 +99,13 @@ class Meteor(pygame.sprite.Sprite):
         rotate_surf = pygame.transform.rotozoom(self.scale_surf, self.rotation, 1)
         self.image = rotate_surf
         self.rect = self.image.get_rect(center = self.rect.center)
+        self.mask = pygame.mask.from_surface(self.image)
 
     def update(self):
         self.pos += self.direction * self.speed * dt
         self.rect.center = (round(self.pos.x), round(self.pos.y))
         self.rotate()
-        if self.rect.top < WINDOW_HEIGHT:
+        if self.rect.top > WINDOW_HEIGHT:
             self.kill()
 
 class Score:
@@ -125,13 +142,14 @@ meteor_group = pygame.sprite.Group()
 
 # sprite creation 
 ship = Ship(ship_group)
-laser = Laser(laser_group, ship.rect.midtop)
 score = Score()
-
 
 # timer
 meteor_timer = pygame.event.custom_type()
 pygame.time.set_timer(meteor_timer, 500)
+
+music = pygame.mixer.Sound('../sounds/music.wav')
+music.set_volume(0.1)
 
 # game loop
 while True:
@@ -163,6 +181,8 @@ while True:
     ship_group.draw(display_surface)
     laser_group.draw(display_surface)
     meteor_group.draw(display_surface)
+
+    music.play(loops = -1)
 
     # draw frame
     pygame.display.update()
